aa520707f18f766975e0b9cab19d249fd8a89995
Naseem Hajoj <naseemhajoj@gmail.com>
2023-07-11 12:03:17 +0300

    Added missing files

---
+++ src/Skoruba.IdentityServer4.STS.Identity/Services/ClientCredentialRequestValidator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+using IdentityServer4.Models;
+using IdentityServer4.Validation;
+
+using Skoruba.IdentityServer4.STS.Identity.Constants;
+
+namespace Skoruba.IdentityServer4.STS.Identity.Services
+{
+    /// <summary>
+    /// Client credential flow validation and augmentation.
+    /// </summary>
+    public class ClientCredentialRequestValidator : ICustomTokenRequestValidator
+    {
+        /// <inheritdoc />
+        public Task ValidateAsync(CustomTokenRequestValidationContext context)
+        {
+            ValidatedTokenRequest validatedTokenRequest = context?.Result?.ValidatedRequest;
+
+            if (validatedTokenRequest?.GrantType == GrantType.ClientCredentials)
+            {
+                // if the flow is client credential, we want to add tenant Id of system and authentication type 
+                // of client credentials as claims
+                var claims = new List<Claim>
+                {
+                    new Claim(Claims.Keys.TenantId, Skoruba.IdentityServer4.Shared.Constants.SystemTenant.Id.ToString()),
+                    new Claim(Claims.Keys.AuthType, Claims.Values.ClientCredentialsAuthType),
+                };
+
+                claims.ToList().ForEach(claim => context.Result.ValidatedRequest.ClientClaims.Add(claim));
+
+                // don't want it to be prefixed with "client_" ? we change it here (or from global settings)
+                context.Result.ValidatedRequest.Client.ClientClaimsPrefix = "";
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Skoruba.IdentityServer4.Shared/Constants.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Skoruba.IdentityServer4.Shared
+{
+    public static class Constants
+    {
+        /// <summary>
+        /// Constants for the System tenant.
+        /// </summary>
+        public static class SystemTenant
+        {
+            /// <summary>
+            /// The tenant ID of the system.
+            /// </summary>
+            public static readonly Guid Id = new Guid("48e6ecd3-d3a5-4da2-a53f-f5921784912a");
+
+            public const string Name = "System";
+
+            public const string DisplayName = "Advanced Packages Tracker System";
+
+            public const string Email = "system@advancedpackagestrack.com";
+
+            public const string Mobile = "+972508312025";
+
+            public const string SystemUserId = "487eeea4-e470-419f-9bc3-c20e148b6a3a";
+
+            public static readonly Guid LicenseKey = new Guid("D021DE7B-2B72-492C-B1D9-953FCB0D6C2B");
+        }
+    }
+}
